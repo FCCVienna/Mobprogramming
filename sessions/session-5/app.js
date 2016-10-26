75af88e04e635b499dfac4d9bcbf71bb476c731b
robeerob <rob@rob.ee>
2016-10-26 17:40:33 +0200

    Complete first working app.js version
    I played around and managed to, crudely, output first valid link
    from userInput url. Feel free to refactor or push your own working
    version.

--- sessions/session-5/app.js
+++ sessions/session-5/app.js
@@ -1,20 +1,36 @@
 const https = require('https');
 const cheerio = require('cheerio');
+const fs = require('fs');
+var link = '';
 
 function getLink() {
   var userInput = process.argv[2];
-  console.log(userInput);
   https.get(userInput, function(res) {
-    //console.log(res);
-    res.setEncoding("utf8");
+    res.setEncoding('utf8');
     res.on("data", function(a) {
       var $ = cheerio.load(a);
-      //var linkArray = $('a').html().split('\n');
-      $('a').attr('href').filter((index,el)=>{
-        if(el){console.log(el)}
-      });
+      // if item is not undefined, and therefor a valid link
+      if ($('a').attr('href') != undefined) {
+        // if link is an empty string and contains the word http
+	    if (link == '' && $('a').attr('href').indexOf('http') != -1) {
+          // write stream to links.txt file
+          fs.appendFile('links.txt', $('a').attr('href') + '\n', (err) => {
+            if (err) throw err;
+          });
+        }
+      }
     });
-    res.on('end',()=>{console.log('response ended')})
+    res.on('end',()=>{
+      // reads first line of links.txt file
+      fs.readFile('links.txt', 'utf8', (err, data) => {
+      if (err) throw err;
+        links = data.split('\n');
+        console.log(links[0]);
+      });
+      // deletes file
+      fs.unlinkSync('links.txt');
+        
+    });
   });
 }
 
